b61a0c7f53c7206e16bb74dd955fe141af8fb77e
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-07 11:10:57 +0900

    chore: init unittest project

---
+++ unitTesting/unitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitTestingApp.swift
+//  unitTesting
+//
+//  Created by CHANG JIN LEE on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct unitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
