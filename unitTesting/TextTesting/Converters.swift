391363231b7ce60414417284bcc9bd8fc3bc0646
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-20 01:22:38 +0900

    feature: update Converters
    feature: update Converters

--- unitTesting/TextTesting/Converters.swift
+++ unitTesting/TextTesting/Converters.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class Converters {
+    func convertKRWToUSD(KRW: String) -> String {
+    let usdRate = 1.08
+    let KRWValue = Double(KRW) ?? 0
+
+        if KRWValue <= 0 {
+            return "Please enter a positive number."
+        }
+        if KRWValue >= 1_000_000 {
+            return "Value too big to convert!"
+        }
+        return "$\(String(format: "%.2f", KRWValue * usdRate))"
+    }
+}
