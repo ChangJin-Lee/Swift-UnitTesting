a1434af94d09565794c07daf2effe17626e28783
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-20 01:22:15 +0900

    feature: update TextTestView
    feature: update TextTestView

--- unitTesting/TextTesting/TextViewModel.swift
+++ unitTesting/TextTesting/TextViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+final class ViewModel: ObservableObject {
+    @Published var text = ""
+    @Published var convertedText = "$0"
+    private let converter = Converters()
+
+    func convertMoney () {
+        self.convertedText = converter.convertKRWToUSD(KRW: text)
+        self.hideKeyboard()
+    }
+
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil,
+                                          from: nil, for: nil)
+    }
+}
